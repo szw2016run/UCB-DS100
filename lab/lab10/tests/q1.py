09a7ca08f809f1f7fbc3032d9974c4f9cebc0f31
Raguvir Kunani <rkunani@berkeley.edu>
2019-07-29 23:17:42 -0700

    release lab10

---
+++ lab/lab10/tests/q1.py
@@ -0,0 +1,10 @@
+test = {   'name': 'q1',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> sum(lr.predict(x_test) == '
+                                               'y_test)\n',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab10/tests/q2.py
@@ -0,0 +1,14 @@
+test = {   'name': 'q2',
+    'points': 1,
+    'suites': [   {   'cases': [   {   'code': '>>> np.isclose(precision, '
+                                               '0.97727272727272729)\n',
+                                       'hidden': False,
+                                       'locked': False},
+                                   {   'code': '>>> np.isclose(recall, '
+                                               '0.79629629629629628)\n',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
