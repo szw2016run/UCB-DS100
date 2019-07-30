bab3cc341101078f78a7ede333b5411de3a1f63f
Raguvir Kunani <rkunani@berkeley.edu>
2019-07-30 12:02:40 -0700

    fix lab10

--- lab/lab10/tests/q2.py
+++ lab/lab10/tests/q2.py
@@ -1,11 +1,13 @@
 test = {   'name': 'q2',
     'points': 1,
     'suites': [   {   'cases': [   {   'code': '>>> np.isclose(precision, '
-                                               '0.97727272727272729)\n',
+                                               '0.97727272727272729)\n'
+                                               'True',
                                        'hidden': False,
                                        'locked': False},
                                    {   'code': '>>> np.isclose(recall, '
-                                               '0.79629629629629628)\n',
+                                               '0.79629629629629628)\n'
+                                               'True',
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
--- lab/lab10/tests/q3.py
+++ lab/lab10/tests/q3.py
@@ -3,7 +3,8 @@ test = {   'name': 'q3',
     'suites': [   {   'cases': [   {   'code': '>>> '
                                                'np.isclose(lr_loss(np.array([0, '
                                                '0]), X_new, Y), '
-                                               '0.69314718055994529)\n',
+                                               '0.69314718055994529)\n'
+                                               'True',
                                        'hidden': False,
                                        'locked': False},
                                    {   'code': '>>> np.isclose(lr_loss(theta,\n'
@@ -12,7 +13,8 @@ test = {   'name': 'q3',
                                                '...                           '
                                                'Y),\n'
                                                '...                   '
-                                               '0.34566168837055461)\n',
+                                               '0.34566168837055461)\n'
+                                               'True',
                                        'hidden': False,
                                        'locked': False}],
                       'scored': True,
